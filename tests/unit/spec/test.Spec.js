a1a70df40c9be5e427e02ade9cfe3ebf25758bbf
asubota <asubota@cogniance.com>
2014-01-17 17:34:44 +0200

    tests for jasmine

---
+++ tests/unit/spec/test.Spec.js
@@ -0,0 +1,11 @@
+describe("Testing...", function(){
+    
+    it("true should be equal true", function(){
+       expect(true).toEqual(true);
+    });
+
+    it("true should not be equal false", function(){
+       expect(true).not.toEqual(false);
+    });    
+
+});
