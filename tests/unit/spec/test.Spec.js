b17f592a5b6c1bb72b17c9c0b08115212f4ebe50
Aleksandr <subotaav@gmail.com>
2014-02-04 20:27:02 +0200

    few dummy tests

--- tests/unit/spec/test.Spec.js
+++ tests/unit/spec/test.Spec.js
@@ -1,11 +1,20 @@
 describe("Testing...", function(){
-    
-    it("true should be equal true", function(){
-       expect(true).toEqual(true);
-    });
 
-    it("true should not be equal false", function(){
-       expect(true).not.toEqual(false);
-    });    
+  it("true should be equal true", function(){
+    expect(true).toEqual(true);
+  });
+
+  it("true should not be equal false", function(){
+    expect(true).not.toEqual(false);
+  });
+
+  it('passes if subject is true', function() {
+    expect({}).toBeTruthy();
+    expect('').not.toBeTruthy();
+  });
+
+  it('compares the actual to the expected using a regular expression', function() {
+    expect('Hello Jasmine').toMatch(/jasmine/i);
+  });
 
 });
